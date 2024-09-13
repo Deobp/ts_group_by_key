c95953ca9863f66ba8bf37ba66ad74d78a3d0c96
Oleksandr <d43z61p0@gmail.com>
2024-09-13 10:59:41 +0300

    init

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,19 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey(
+  items: Array<string | number>,
+  key: string | number,
+): GroupsMap {
+  const result: GroupsMap = {};
+
+  for (const item of items) {
+    if (result.hasOwnProperty(item[key])) {
+      result[item[key]].push(item);
+    } else {
+      result[item[key]] = [item];
+    }
+  }
+
+  return result;
 }
